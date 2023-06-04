cce1ae689148a2489456901aab7cd26891941a96
digithuman <cgd321@gmail.com>
2023-06-04 17:06:50 +0800

    类型判断

--- trunk/src/main/java/com/crenjoy/proto/utils/TypeUtil.java
+++ trunk/src/main/java/com/crenjoy/proto/utils/TypeUtil.java
@@ -5,7 +5,11 @@ import com.crenjoy.proto.beanutils.ProtoConvertUtils;
 import com.crenjoy.proto.mapper.BeanMapper;
 import com.crenjoy.proto.mapper.ProtoMapper;
 import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.Timestamp;
 import com.google.protobuf.util.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.LogFactory;
 
 /**
@@ -45,6 +49,23 @@ public class TypeUtil implements BeanMapper, ProtoMapper {
    */
   public static Object enumToObject(Enum<?> enumValue, Class<?> targetClass) {
     return ProtoConvertUtils.convert(enumValue, targetClass);
+  }
+
+  public static boolean isEmpty(Timestamp timestamp) {
+    return null == timestamp || ((timestamp.getSeconds() == 0L) && (timestamp.getNanos() == 0L));
+  }
+
+  public static boolean isEmpty(LocalDateTime localDateTime) {
+    return null == localDateTime;
+  }
+
+  public static boolean isEmpty(UUID uuid) {
+    return null == uuid
+        || StringUtils.equals("00000000-0000-0000-0000-000000000000", uuid.toString());
+  }
+
+  public static boolean isEmpty(String str) {
+    return StringUtils.isBlank(str);
   }
 
 }
