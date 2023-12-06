ac4a02d5e639b1f6c385d3a24eb2eac23312724e
CGD <52398508@qq.com>
2023-12-06 16:02:21 +0800

    UUID 支持32 位

--- trunk/src/main/java/com/crenjoy/proto/beanutils/converters/UUIDConverter.java
+++ trunk/src/main/java/com/crenjoy/proto/beanutils/converters/UUIDConverter.java
@@ -1,18 +1,16 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.crenjoy.proto.beanutils.converters;
@@ -21,30 +19,27 @@ import java.util.UUID;
 import org.apache.commons.beanutils.converters.AbstractConverter;
 
 /**
- * {org.apache.commons.beanutils2.Converter} implementation that handles
- * conversion to and from <b>java.util.UUID</b> objects.
- * <br/>
- * Can be configured to either return a <i>default value</i> or throw a
- * {@code ConversionException} if a conversion error occurs.
+ * {org.apache.commons.beanutils2.Converter} implementation that handles conversion to and from
+ * <b>java.util.UUID</b> objects. <br/>
+ * Can be configured to either return a <i>default value</i> or throw a {@code ConversionException}
+ * if a conversion error occurs.
  *
  * @since 2.0
  */
 public final class UUIDConverter extends AbstractConverter {
 
   /**
-   * Constructs a <b>java.util.UUID</b> <i>Converter</i> that throws a
-   * {@code ConversionException} if an error occurs.
+   * Constructs a <b>java.util.UUID</b> <i>Converter</i> that throws a {@code ConversionException}
+   * if an error occurs.
    */
-  public UUIDConverter() {
-  }
+  public UUIDConverter() {}
 
   /**
-   * Constructs a <b>java.util.UUID</b> <i>Converter</i> that returns a default
-   * value if an error occurs.
+   * Constructs a <b>java.util.UUID</b> <i>Converter</i> that returns a default value if an error
+   * occurs.
    *
-   * @param defaultValue The default value to be returned if the value to be
-   *                     converted is missing or an error occurs converting the
-   *                     value.
+   * @param defaultValue The default value to be returned if the value to be converted is missing or
+   *        an error occurs converting the value.
    */
   public UUIDConverter(final UUID defaultValue) {
     super(defaultValue);
@@ -66,8 +61,8 @@ public final class UUIDConverter extends AbstractConverter {
    * Converts a java.util.UUID or object into a String.
    * </p>
    *
-   * @param <T>   Target type of the conversion.
-   * @param type  Data type to which this value should be converted.
+   * @param <T> Target type of the conversion.
+   * @param type Data type to which this value should be converted.
    * @param value The input value to be converted.
    * @return The converted value.
    * @throws Throwable if an error occurs converting to the specified type
@@ -76,7 +71,12 @@ public final class UUIDConverter extends AbstractConverter {
   @Override
   protected <T> T convertToType(final Class<T> type, final Object value) throws Throwable {
     if (UUID.class.equals(type)) {
-      return type.cast(UUID.fromString(String.valueOf(value)));
+      String uuid = String.valueOf(value);
+      if (uuid.length() == 32) {
+        uuid = String.format("%s-%s-%s-%s-%s", uuid.substring(0, 8), uuid.substring(8, 12),
+            uuid.substring(12, 16), uuid.substring(16, 20), uuid.substring(20));
+      }
+      return type.cast(UUID.fromString(uuid));
     }
 
     throw conversionException(type, value);
